354ae9fb3ec1ebf79e9e9d51938aaddf0649e2bc
Alina Pykus <alinapykus17@email.com>
2022-08-09 21:35:45 +0300

    add some changes

---
+++ main.js
@@ -0,0 +1,91 @@
+// Task 1
+// function calcRectangleArea(width, height) {
+//     try {
+//         if (!Number(width, height)) {
+//         throw 'not a num'
+//         }
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// calcRectangleArea(1, 'a');
+
+// Task 2
+// function checkAge(age) {
+//     prompt('how old are you?', age);
+//     try { 
+//         if  (!Number(age)) {
+//             throw 'not a numb';
+//         }
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+//     try {
+//         if (age > 14) {
+//             throw 'not rigth age';
+//         }
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// Task 3
+// class MonthException {
+//     constructor(month) {
+//         this.month = month;
+//     } 
+// }
+//     const name = ['sep', 'oct', 'nov'];
+
+//     const monthNum = new MonthException()
+//     for(let i = 0; i < name.length; i++) {
+//         return monthNum[i] = name[i];
+//     }
+//     function showMonthName(month) {
+//         let a = Number(prompt('enter'));
+//          try {
+//         if (a = !Number) {
+//             throw 'not a number'
+//         }
+//         } catch (error) {
+//             console.log(error)
+//         }
+//         return this
+//     }
+// showMonthName();
+    
+
+// Task 4
+// const id = [7, -12, 44, 22];
+
+// function showUser(id) {
+//     let a = Number(prompt('entre num'))
+//     for(let i = 0; i < id.length; i++) {
+//         return id[i];
+//     }
+//     try {
+//         if (a == !Number) {
+//             throw 'not a num'
+//         }
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// showUser();
+
+//  const ids = [ {id: 7}, {id: 44}, {id: 22} ];
+
+// function showUsers(ids) {
+// for (let i = 0; i < ids.length; i++) {
+//     return ids[i] = id[i];
+// }
+// try {
+//     if(ids != id) {
+//         throw 'its not rigth'
+//     }
+// } catch (error) {
+//     console.log(error)
+// }
+// }
+// showUsers();
